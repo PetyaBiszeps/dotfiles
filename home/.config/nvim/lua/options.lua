42d7fe7a4df9a0f0cd725ede2e06017c450c2f43
YP00 <personalYP00@icloud.com>
2026-05-19 20:47:48 +0300

    feat: nvim options added

---
+++ home/.config/nvim/lua/options.lua
@@ -0,0 +1,21 @@
+-- Editor options
+
+-- Lines
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "120"
+
+-- Indentation
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+-- Scrolling
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+-- Wrapping
+vim.opt.wrap = false
