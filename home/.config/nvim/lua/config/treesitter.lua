6c7f80f67ca8996cd814fda03f8603072ab3ce58
YP00 <personalYP00@icloud.com>
2026-05-19 22:36:28 +0300

    feat: treesitter nvim

--- home/.config/nvim/lua/config/treesitter.lua
+++ home/.config/nvim/lua/config/treesitter.lua
@@ -1,39 +1,40 @@
 local M = {}
 
+local parsers = {
+  "bash",
+  "css",
+  "go",
+  "html",
+  "javascript",
+  "json",
+  "latex",
+  "lua",
+  "markdown",
+  "markdown_inline",
+  "nix",
+  "python",
+  "regex",
+  "scss",
+  "svelte",
+  "tsx",
+  "typescript",
+  "typst",
+  "vue"
+}
+
 function M.setup()
-  require("nvim-treesitter.configs").setup({
-    ensure_installed = {
-      "bash",
-      "css",
-      "go",
-      "html",
-      "javascript",
-      "json",
-      "latex",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "nix",
-      "python",
-      "regex",
-      "scss",
-      "svelte",
-      "tsx",
-      "typescript",
-      "typst",
-      "vue"
-    },
+  local treesitter = require("nvim-treesitter")
 
-    sync_install = false,
-    auto_install = true,
+  treesitter.install(parsers)
 
-    highlight = {
-      enable = true
-    },
+  vim.api.nvim_create_autocmd("FileType", {
+    callback = function(args)
+      local ok = pcall(vim.treesitter.start, args.buf)
 
-    indent = {
-      enable = true
-    }
+      if not ok then
+        return
+      end
+    end
   })
 end
 
