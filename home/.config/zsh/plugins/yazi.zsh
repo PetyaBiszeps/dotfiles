f804d89072d1509c68daff22bd4f55fd4ff50589
YP00 <PersonalYP00@icloud.com>
2026-05-22 15:36:25 +0300

    feat: yazi zsh config adjustments

--- home/.config/zsh/plugins/yazi.zsh
+++ home/.config/zsh/plugins/yazi.zsh
@@ -1,6 +1,24 @@
 # ~/.config/zsh/plugins/yazi.zsh
 
 if command_exists yazi; then
-  alias E="yazi"
-  alias e="yazi"
+  e() {
+    local tmp cwd
+
+    tmp="$(mktemp -t yazi-cwd.XXXXXX)"
+
+    yazi "$@" --cwd-file="$tmp"
+
+    if [[ -f "$tmp" ]]; then
+      cwd="$(cat "$tmp")"
+      rm -f "$tmp"
+
+      if [[ -n "$cwd" && "$cwd" != "$PWD" ]]; then
+        cd "$cwd" || return
+      fi
+    fi
+  }
+
+  E() {
+    e "$@"
+  }
 fi
