0ead81d8a73452f09aa43b4f7c598fe849bd9d45
YP00 <personalYP00@icloud.com>
2026-05-22 03:39:57 +0300

    fix: keymap issues part I

--- home/.config/zsh/plugins/yazi.zsh
+++ home/.config/zsh/plugins/yazi.zsh
@@ -1,20 +1,5 @@
 # ~/.config/zsh/plugins/yazi.zsh
 
 if command_exists yazi; then
-  e() {
-    local target="${1:-.}"
-
-    if [[ $# -eq 0 ]]; then
-      yazi
-      return
-    fi
-
-    if command_exists zoxide; then
-      local dir
-      dir="$(zoxide query "$target" 2>/dev/null)" || return
-      yazi "$dir"
-    else
-      yazi "$target"
-    fi
-  }
+  alias e="yazi"
 fi
