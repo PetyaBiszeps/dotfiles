0ebe6feefae0c8bc5174cf83a6168b4f519eddc3
YP00 <PersonalYP00@icloud.com>
2026-05-22 15:25:14 +0300

    feat: add bat configuration part II

--- home/.config/zsh/plugins/bat.zsh
+++ home/.config/zsh/plugins/bat.zsh
@@ -2,11 +2,13 @@
 
 if command_exists bat; then
   alias cat="bat"
+  export BAT_PAGER="less -FR"
   export MANPAGER="sh -c 'col -bx | bat -l man -p'"
   export MANROFFOPT="-c"
 elif command_exists batcat; then
   alias bat="batcat"
   alias cat="batcat"
+  export BAT_PAGER="less -FR"
   export MANPAGER="sh -c 'col -bx | batcat -l man -p'"
   export MANROFFOPT="-c"
 fi
